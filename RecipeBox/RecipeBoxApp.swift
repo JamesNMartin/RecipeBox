466b1961f293046751f1c91662e3ab093efdc16c
James Martin <jamesnicholasmartin@gmail.com>
2021-12-31 12:00:24 -0600

    Initial Commit

---
+++ RecipeBox/RecipeBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBoxApp.swift
+//  RecipeBox
+//
+//  Created by James Martin on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
